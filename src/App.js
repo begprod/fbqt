6f76ffbeda08d3d2c9428770323eafbca464d672
Andrey Aratov <madingame@yandex.ru>
2018-06-25 22:23:25 +0300

    ie 11 / fix

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Productgrid from './components/Productgrid/Productgrid';
 class App extends Component {
     render() {
         return (
-            <div className="section">
-                <div className="section__inner">
-                    <h1 className="section__head">Ты сегодня покормил кота?</h1>
-                    <Productgrid/>
+            <div className="wrapper">
+                <div className="section">
+                    <div className="section__inner">
+                        <h1 className="section__head">Ты сегодня покормил кота?</h1>
+                        <Productgrid/>
+                    </div>
                 </div>
             </div>
         );
